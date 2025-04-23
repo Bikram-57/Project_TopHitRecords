2e1734949f46230a3a6c81be2813d9a9b54652f6
Bikram-57 <bikramdas20032@gmail.com>
2025-04-23 22:53:13 +0530

    design register page

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 function whatsapp() {
-    var phoneNumber = "+919800314116";
+    var phoneNumber = "+916297103889";
     var message = "Hello, I want to book a stay!";
     var apiLink = "https://api.whatsapp.com/send?phone=" + phoneNumber + "&text=" + encodeURIComponent(message);
     window.open(apiLink, "_blank");
